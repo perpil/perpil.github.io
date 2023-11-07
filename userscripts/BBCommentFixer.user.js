55bc137879c8e6b23ff4908683b975420f6bd26d
perpil <97474956+perpil@users.noreply.github.com>
2023-11-07 09:34:43 -0800

    Update BBCommentFixer.user.js

--- userscripts/BBCommentFixer.user.js
+++ userscripts/BBCommentFixer.user.js
@@ -2,12 +2,14 @@
 // @name         BB Comment Fixer
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  Fixes broken comments link on BoingBoing
-// @author       You
+// @description  Fixes broken comments links on BoingBoing
+// @author       David Behroozi
 // @match        https://boingboing.net
 // @include      /^https:\/\/boingboing.net\/\d{4}\/\d{2}\/\d{2}/[^\/]+\.html$/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=boingboing.net
 // @grant        none
+// @updateURL    https://perpil.nobackspacecrew.com/userscripts/BBCommentFixer.user.js
+// @downloadURL  https://perpil.nobackspacecrew.com/userscripts/BBCommentFixer.user.js
 // ==/UserScript==
 
 (function() {
@@ -18,6 +20,8 @@
         let href = bbs.href;
         let title = document.querySelector('h1.entry-title')?.textContent;
         let hasWeirdChars = title && title.match(/[\/*:]/);
-        bbs.href=href.includes('bbs')?href:'https://bbs.boingboing.net/t/' + (hasWeirdChars?title.replaceAll(/[^\w: *]/g,'').replaceAll(/[\W]+/g,'-').toLowerCase():/.*\/(.*)\.html$/.exec(location.pathname)[1]);
+        bbs.href = href.includes('bbs')?href : 'https://bbs.boingboing.net/t/' + (hasWeirdChars ?
+                                                                                title.replaceAll(/[^\w: *]/g,'').replaceAll(/[\W]+/g,'-').toLowerCase() :
+                                                                                /.*\/(.*)\.html$/.exec(location.pathname)[1]);
     }
 })();

0305f47f08e542885ade13d8bda999edfc58777b
perpil <97474956+perpil@users.noreply.github.com>
2023-11-08 08:54:25 -0800

    Update BBCommentFixer.user.js

--- userscripts/BBCommentFixer.user.js
+++ userscripts/BBCommentFixer.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         BB Comment Fixer
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.01
 // @description  Fixes broken comments links on BoingBoing
-// @author       David Behroozi
+// @author       dangaroo
 // @match        https://boingboing.net
 // @include      /^https:\/\/boingboing.net\/\d{4}\/\d{2}\/\d{2}/[^\/]+\.html$/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=boingboing.net
@@ -20,7 +20,7 @@
         let href = bbs.href;
         let title = document.querySelector('h1.entry-title')?.textContent;
         let hasWeirdChars = title && title.match(/[\/*:]/);
-        bbs.href = href.includes('bbs')?href : 'https://bbs.boingboing.net/t/' + (hasWeirdChars ?
+        bbs.href = href.includes('https://bbs.boingboing.net/t/') ? href : 'https://bbs.boingboing.net/t/' + (hasWeirdChars ?
                                                                                 title.replaceAll(/[^\w: *]/g,'').replaceAll(/[\W]+/g,'-').toLowerCase() :
                                                                                 /.*\/(.*)\.html$/.exec(location.pathname)[1]);
     }

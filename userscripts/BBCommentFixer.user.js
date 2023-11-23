f203d4c70b501ce9181600dd413a5a7554860b35
perpil <97474956+perpil@users.noreply.github.com>
2023-11-22 21:24:46 -0800

    Bugfix for / in title.

--- userscripts/BBCommentFixer.user.js
+++ userscripts/BBCommentFixer.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         BB Comment Fixer
 // @namespace    http://tampermonkey.net/
-// @version      1.02
+// @version      1.03
 // @description  Fixes broken comments links on BoingBoing
-// @author       dangaroo
+// @author       David Behroozi
 // @match        https://boingboing.net
 // @include      /^https:\/\/boingboing.net\/\d{4}\/\d{2}\/\d{2}/[^\/]+\.html$/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=boingboing.net
@@ -21,7 +21,7 @@
         let title = document.querySelector('h1.entry-title')?.textContent;
         let hasWeirdChars = title && title.match(/[\/*:]/);
         bbs.href = href.includes('https://bbs.boingboing.net/t/') ? href : 'https://bbs.boingboing.net/t/' + (hasWeirdChars ?
-                                                                                title.replaceAll(/[^-\w: *]/g,'').replaceAll(/[\W]+/g,'-').toLowerCase() :
+                                                                                title.replaceAll(/[^-\/\w: *]/g,'').replaceAll(/[\W]+/g,'-').toLowerCase() :
                                                                                 /.*\/(.*)\.html$/.exec(location.pathname)[1]);
     }
 })();
